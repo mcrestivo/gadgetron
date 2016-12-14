63b1004adc70e4044de479813e43aca8236f0be3
matthew restivo <matthew.restivo@nih.gov>
2016-12-14 16:50:20 -0500

    Added standalone deblurring app

--- gadgets/spiral/gpuSpiralDeblurGadget.cpp
+++ gadgets/spiral/gpuSpiralDeblurGadget.cpp
@@ -247,7 +247,7 @@ typedef cuNFFT_plan<_real,2> plan_type;
     //
 
 	GPUTimer *timer;
-	timer = new GPUTimer("Process time");
+	//timer = new GPUTimer("Process time");
 	int flag = m1->getObjectPtr()->user_int[0];
 	if(flag > 0){
 		Nints = 1;
@@ -667,6 +667,7 @@ typedef cuNFFT_plan<_real,2> plan_type;
 		hoNDArray<_complext> samples_demod( new hoNDArray<_complext> );
 		int j = 0;
 		//int indx = 0;
+		//timer = new GPUTimer("MFI Deblurring");
 		for(float f = -fmax; f <= fmax; f += fmax*2./(L-1)){
 			samples_demod = host_data_buffer_[set*slices_+slice];
 			_complext omega = _complext(0,2*M_PI*f*sample_time);
@@ -681,9 +682,15 @@ typedef cuNFFT_plan<_real,2> plan_type;
 			}
 			//delete timer;
 			samples = samples_demod;
+			timer = new GPUTimer("nfft compute");
 			nfft_plan_.compute( &samples, &image, dcw_buffer_.get(), plan_type::NFFT_BACKWARDS_NC2C );
+			delete timer;
+			timer = new GPUTimer("csm_conj_sum");
 			E->mult_csm_conj_sum( &image, &reg_image );
+			delete timer;
+			timer = new GPUTimer("to host");
 			temp_image = *(reg_image.to_host());
+			delete timer;
 			//int i;
 			//timer = new GPUTimer("MFI Combination");
 			#ifdef USE_OMP
@@ -697,7 +704,7 @@ typedef cuNFFT_plan<_real,2> plan_type;
 			//delete timer;
 			j++;
 		}
-		
+		//delete timer;
 		//write_nd_array<_complext>( &output_image, "deblurred_im.cplx" );
 		reg_image = output_image;
 	}
@@ -725,7 +732,7 @@ typedef cuNFFT_plan<_real,2> plan_type;
       interleaves_counter_singleframe_[set*slices_+slice] = 0;
     }
     m1->release();
-	delete timer;
+	//delete timer;
     return GADGET_OK;
   }
 
